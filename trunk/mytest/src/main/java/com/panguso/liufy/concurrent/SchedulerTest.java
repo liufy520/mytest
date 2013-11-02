b603ec62e61ab9c090ce23656058b4313651d30c
liufy520 <lfy_flying@hotmail.com>
2013-11-02 21:22:23 +0800

    Signed-off-by: liufy520 <lfy_flying@hotmail.com>

--- trunk/mytest/src/main/java/com/panguso/liufy/concurrent/SchedulerTest.java
+++ trunk/mytest/src/main/java/com/panguso/liufy/concurrent/SchedulerTest.java
@@ -9,6 +9,7 @@
  */
 package com.panguso.liufy.concurrent;
 
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ThreadFactory;
@@ -16,6 +17,8 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
+ * ScheduledExecutorService当一个任务抛出异常，后面的任务就不执行了；对于ExecutorService，如果一个任务线程抛出异常，
+ * 则会new新的线程来处理后面的任务。
  * 
  * @author panguso
  * @date 2013年11月2日
@@ -28,24 +31,49 @@ public class SchedulerTest {
 	 * @date 2013年11月2日
 	 */
 	public static void main(String[] args) {
-		ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor(new ThreadFactory() {
-			AtomicInteger i = new AtomicInteger(0);
-			@Override
-			public Thread newThread(Runnable r) {
-				Thread thread = new Thread(r);
-				thread.setName("t-" + i.incrementAndGet());
-				return thread;
-			}
-		});
-		
+		ScheduledExecutorService executorService = Executors
+		        .newSingleThreadScheduledExecutor(new ThreadFactory() {
+			        AtomicInteger i = new AtomicInteger(0);
+
+			        @Override
+			        public Thread newThread(Runnable r) {
+				        Thread thread = new Thread(r);
+				        thread.setName("t-" + i.incrementAndGet());
+				        return thread;
+			        }
+		        });
+
 		executorService.scheduleAtFixedRate(new Runnable() {
-			
+
 			@Override
 			public void run() {
 				System.out.println("线程进入");
 				throw new RuntimeException("线程出错了");
 			}
 		}, 1, 2, TimeUnit.SECONDS);
+
+		ScheduledExecutorService executorService1 = Executors.newScheduledThreadPool(2);
+
+		executorService1.scheduleAtFixedRate(new Runnable() {
+
+			@Override
+			public void run() {
+				System.out.println("线程进入2");
+				throw new RuntimeException("");
+			}
+		}, 1, 2, TimeUnit.SECONDS);
+
+		ExecutorService executorService2 = Executors.newFixedThreadPool(2);
+		for (int i = 0; i < 10; i++) {
+			executorService2.submit(new Runnable() {
+
+				@Override
+				public void run() {
+					System.out.println("任务3开始");
+					throw new RuntimeException("");
+				}
+			});
+		}
 	}
 
 }
